9de21326810eef9f16cc81c01ed72c1de114b27f
Gianmarco G <giangarof98@gmail.com>
2022-02-04 14:31:56 -0500

    add start script

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err})
 })
 
-app.listen(3000, () => {
-    console.log('server connected!')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`port: ${port}`)
 })
