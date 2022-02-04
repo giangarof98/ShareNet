6c2776e342c50eb655bfc4f9f917952b7ab76dff
Gianmarco G <giangarof98@gmail.com>
2022-02-04 18:45:04 -0500

    password

--- index.js
+++ index.js
@@ -26,16 +26,14 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
-const secret = process.env.SECRET || 'secret!';
+const secret = process.env.SECRET || 'secret';
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/sharenet';
 // const secret = process.env.SECRET || 'secret';
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
-    crypto: {
-        secret: 'secret',
-    }
+    secret: 'secret',
 })
 
 store.on('error', function(e) {
