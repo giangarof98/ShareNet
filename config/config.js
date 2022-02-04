6a47a6a935217a43b1509c9ec035adbe5aa10e5f
Gianmarco G <giangarof98@gmail.com>
2022-02-04 17:32:11 -0500

    connection

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const Content = require('../models/content');
 const mongoose = require('mongoose')
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/sharenet';
 // const dbUrl = 'mongodb://localhost:27017/sharenet'
 
 mongoose.connect(dbUrl, {
